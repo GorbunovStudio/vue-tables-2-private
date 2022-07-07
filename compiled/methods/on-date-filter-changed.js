26f2f9a6d1d2ae51b3fec80d0fc7f8bdb446582c
Anton Sylka <anton.sylka@gorbunov.studio>
2022-07-07 11:41:11 +0800

    Refs #26915: Build

--- compiled/methods/on-date-filter-changed.js
+++ compiled/methods/on-date-filter-changed.js
@@ -1,13 +1,20 @@
 "use strict";
 
 module.exports = function (_ref) {
+  var _this = this;
+
   var date = _ref.date,
       column = _ref.column;
 
-  var search = function search(query, e) {
-    return this.source == 'client' ? this.search(that.data, e) : this.serverSearch(query, e);
+  var search = function search(e) {
+    return _this.source == 'client' ? _this.search(that.data, e) : _this.serverSearch(_this.query, e);
   };
 
   this.query[column] = "".concat(date.getFullYear(), "-").concat(date.getMonth() + 1, "-").concat(date.getDate(), " 23:59:59");
-  search();
+  search({
+    target: {
+      name: this._getColumnName(column),
+      value: query[column]
+    }
+  });
 };
