be8d974c59d4dd58ea3b02642fcd299e923d535d
Anton Sylka <anton.sylka@gorbunov.studio>
2022-07-11 17:30:03 +0800

    Refs #26915: fix daterangepicker init

--- compiled/methods/init-date-filters.js
+++ compiled/methods/init-date-filters.js
@@ -2,6 +2,10 @@
 
 var merge = require('merge');
 
+require('jquery');
+
+require('daterangepicker');
+
 module.exports = function () {
   if (typeof $ === 'undefined' || typeof $(this.$el).daterangepicker === 'undefined') {
     console.error('Date filters require jquery and daterangepicker');
